bf7fc92e376636e9b0b0aae5fc0ff690fef0e7d6
Ruben Martinez Jr. <rmartin@bowdoin.edu>
2014-02-01 19:20:28 -0500

    Style Improvements, bump to 1.2

--- get_menu.js
+++ get_menu.js
@@ -4,10 +4,13 @@
 var now = new Date();
 var hours = now.getHours();
 var day = now.getDay();
-if(hours < 11 && day <= 5) $('#meal').val('Breakfast');
+if(hours < 11 && day > 0 && day < 6)
+    $('#meal').val('Breakfast');
 else if(hours < 14) {
-    if(day > 5) $('#meal').val('Brunch');
-    else        $('#meal').val('Lunch');
+    if(day === 0 || day === 6)
+        $('#meal').val('Brunch');
+    else
+        $('#meal').val('Lunch');
 }
 else $('#meal').val('Dinner');
 load_menu();
@@ -15,5 +18,7 @@ $('#meal,#unit').change(function(){
     load_menu();
 });
 function load_menu() {
-    $('.menus').html('<div class="spinner">B</div>').load('http://diningwithstrangers.co/extension/get_menu.php?meal='+$('#meal').val()+'&unit='+$('#unit').val());
+    $('.menus').html('<div class="spinner">B</div>').load('http://diningwithstrangers.co/extension/get_menu.php?meal='+$('#meal').val()+'&unit='+$('#unit').val(), function() {
+        $('.menus').find('strong, hr').remove();
+    });
 }
